750213721ab597284691414c1b7dc91251ad2aed
Govindvarshney001 <Govind.work001@example.com>
2025-10-29 00:11:37 +0530

    Frontend Done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,11 @@ import { Toaster } from 'react-hot-toast'
 import { useLocation } from 'react-router-dom'
 import HeroSection from './components/HeroSection'
 import Footer from './components/Footer'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
 
 const App = () => {
    const isAdminRoute = useLocation().pathname.startsWith("/admin");
@@ -25,7 +30,15 @@ const App = () => {
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBookings />} />
         <Route path="/Favorite" element={<Favorite />} />
+        <Route path="/admin/*" element={<Layout />} >
+           <Route index element={<Dashboard/>} />
+           <Route path="add-shows" element={<AddShows/>} />
+           <Route path="list-shows" element={<ListShows/>} />
+           <Route path="list-bookings" element={<ListBookings/>} />
+        </Route>
+
       </Routes>
+      
       {!isAdminRoute && <Footer />}
     </>
   );
